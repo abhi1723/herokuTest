eaaf8ae9dd85eb4740c2ead8b408fa24c27387e4
Abhishek <abhishekisme1996@gmail.com>
2020-12-06 01:18:45 +0530

    Code setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app =require('./app');
 dotenv.config({path: './config.env'});
 const DB = process.env.DATABASE;
+
 mongoose
     .connect(DB,{
         useNewUrlParser: true,
